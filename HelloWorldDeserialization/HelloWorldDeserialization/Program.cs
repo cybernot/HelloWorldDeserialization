803dc2c27ea5200b2b36b393ee9eae9aae562fc6
Louis Nadeau <louis.nadeau@gmail.com>
2017-12-15 10:40:57 -0500

    Initial COmmit

---
+++ HelloWorldDeserialization/HelloWorldDeserialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldDeserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
